a87834e5979146c24dcb863a3fa11784a5f94de7
loliveto <olivetolt@verizon.net>
2018-04-18 10:58:28 -0400

    answersheet

--- app/src/main/java/com/example/john/quizsurvey/DataModels/Answersheet.java
+++ app/src/main/java/com/example/john/quizsurvey/DataModels/Answersheet.java
@@ -1,8 +1,5 @@
 package com.example.john.quizsurvey.DataModels;
 
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -14,7 +11,6 @@ public class Answersheet {
     public ArrayList<String> correctAnswers = new ArrayList<String>();
     public HashMap<Integer, ArrayList<String>> userAnswers = new  HashMap<Integer, ArrayList<String>>();
     public int id;
-    public String firebase_id = "";
 
 //    public AnswerSheet(){;
 //    }
@@ -49,13 +45,5 @@ public class Answersheet {
 
     public ArrayList<String> getUserAnswers(Integer id) {
         return userAnswers.get(id);
-    }
-
-    public void save() {
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        //This questionare has been saved before, and this is an edit, so overwrite the current data on it in firebase
-        DatabaseReference myRef = database.getReference("questionares/" + firebase_id + "/asheet");
-        //Save this object at its proper location
-        myRef.setValue(this);
     }
 }
