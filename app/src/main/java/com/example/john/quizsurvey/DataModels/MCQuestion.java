8df70b6fb79692369f58a9ff8cb4d08b3df92956
Nicole Hilden <nicole.hilden@gmail.com>
2018-04-18 11:34:01 -0400

    mc

--- app/src/main/java/com/example/john/quizsurvey/DataModels/MCQuestion.java
+++ app/src/main/java/com/example/john/quizsurvey/DataModels/MCQuestion.java
@@ -4,5 +4,18 @@ package com.example.john.quizsurvey.DataModels;
  * Created by Laura on 4/18/2018.
  */
 
-public class MCQuestion {
+public class MCQuestion extends Question{
+
+    public MCQuestion(String p)
+    {
+        prompt = p;
+        type = "MC";
+        setOption();
+    }
+
+    @Override
+    public void setOption(){
+//        options.add("True");
+//        options.add("False");
+    }
 }
---
+++ app/src/main/java/com/example/john/quizsurvey/Views/CreateMCQuestion.java
@@ -0,0 +1,80 @@
+package com.example.john.quizsurvey.Views;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+
+import com.example.john.quizsurvey.DataModels.Question;
+import com.example.john.quizsurvey.DataModels.Questionare;
+import com.example.john.quizsurvey.DataModels.MCQuestion;
+import com.example.john.quizsurvey.DataModels.TFQuestion;
+import com.example.john.quizsurvey.MainActivity;
+import com.example.john.quizsurvey.R;
+
+/**
+ * Created by Test on 4/18/2018.
+ */
+
+public class CreateMCQuestion extends Fragment {
+
+    Questionare questionare;
+
+    public CreateMCQuestion() {
+        // Required empty public constructor
+    }
+
+    public static CreateMCQuestion newInstance(Questionare q) {
+        CreateMCQuestion fragment = new CreateMCQuestion();
+        fragment.questionare = q;
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view =  inflater.inflate(R.layout.create_mcquestion, container, false);
+
+        final EditText prompt = view.findViewById(R.id.mcprompt);
+
+        Button submit = view.findViewById(R.id.submitMC);
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MCQuestion question = new MCQuestion(prompt.getText().toString());
+                questionare.addQuestion(question);
+                ((MainActivity)getActivity()).toSeeQuestionare(questionare);
+            }
+        });
+        return view;
+    }
+
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.TrueButton:
+                if (checked)
+                    questionare.asheet.addCorrectAnswer("True");
+                break;
+            case R.id.FalseButton:
+                if (checked)
+                    questionare.asheet.addCorrectAnswer("False");
+                break;
+        }
+    }
+
+}
