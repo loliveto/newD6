25c3130e63457fc50fc8c717ddadf4f7f79b435d
loliveto <olivetolt@verizon.net>
2018-04-25 10:42:02 -0400

    created short question files

--- app/src/main/java/com/example/john/quizsurvey/DataModels/ShortQuestion.java
+++ app/src/main/java/com/example/john/quizsurvey/DataModels/ShortQuestion.java
@@ -4,5 +4,11 @@ package com.example.john.quizsurvey.DataModels;
  * Created by Laura on 4/18/2018.
  */
 
-public class ShortQuestion {
+public class ShortQuestion extends Question{
+    public ShortQuestion(String p)
+    {
+        prompt = p;
+        type = "SQ";
+    }
+
 }
--- app/src/main/java/com/example/john/quizsurvey/Views/CreateShortQuestion.java
+++ app/src/main/java/com/example/john/quizsurvey/Views/CreateShortQuestion.java
@@ -1,32 +1,32 @@
 package com.example.john.quizsurvey.Views;
 
-
+import android.app.Fragment;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
-import android.widget.RadioGroup;
 
 import com.example.john.quizsurvey.DataModels.Questionare;
+import com.example.john.quizsurvey.DataModels.ShortQuestion;
 import com.example.john.quizsurvey.DataModels.TFQuestion;
 import com.example.john.quizsurvey.MainActivity;
 import com.example.john.quizsurvey.R;
 
+/**
+ * Created by Laura on 4/25/2018.
+ */
 
-public class CreateTFQuestion extends Fragment {
-
+public class CreateShortQuestion extends Fragment {
     Questionare questionare;
 
-    public CreateTFQuestion() {
-        // Required empty public constructor
+    public CreateShortQuestion(){
     }
 
-    public static CreateTFQuestion newInstance(Questionare q) {
-        CreateTFQuestion fragment = new CreateTFQuestion();
+    public static CreateShortQuestion newInstance(Questionare q) {
+        CreateShortQuestion fragment = new CreateShortQuestion();
         fragment.questionare = q;
         return fragment;
     }
@@ -40,24 +40,17 @@ public class CreateTFQuestion extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view =  inflater.inflate(R.layout.fragment_create_tfquestion, container, false);
-
-        final EditText prompt = view.findViewById(R.id.TFQuestionPrompt);
-
-        Button submit = view.findViewById(R.id.submitTF);
-        final RadioButton tbutton = view.findViewById(R.id.TrueButton);
-        final RadioButton fbutton = view.findViewById(R.id.FalseButton);
+        View view =  inflater.inflate(R.layout.fragment_create_short, container, false);
+        final EditText prompt = view.findViewById(R.id.prompt);
+        final EditText answer = view.findViewById(R.id.shortanswer);
+        Button submit = view.findViewById(R.id.submitshort);
 
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                TFQuestion question = new TFQuestion(prompt.getText().toString());
+                ShortQuestion question = new ShortQuestion(prompt.getText().toString());
                 questionare.addQuestion(question);
-                if(tbutton.isChecked()){
-                    questionare.asheet.addCorrectAnswer("True");
-                }else if(fbutton.isChecked()){
-                    questionare.asheet.addCorrectAnswer("False");
-                }
+                questionare.asheet.addCorrectAnswer(answer.getText().toString());
                 ((MainActivity)getActivity()).toSeeQuestionare(questionare);
             }
         });
@@ -65,5 +58,4 @@ public class CreateTFQuestion extends Fragment {
 
         return view;
     }
-
 }
