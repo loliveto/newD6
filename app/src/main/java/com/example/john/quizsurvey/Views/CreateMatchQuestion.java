60f9ce60b88bc4400ff0566095b271243e33350e
loliveto <olivetolt@verizon.net>
2018-04-26 18:25:33 -0400

    updated match question

--- app/src/main/java/com/example/john/quizsurvey/Views/CreateMatchQuestion.java
+++ app/src/main/java/com/example/john/quizsurvey/Views/CreateMatchQuestion.java
@@ -1,8 +1,19 @@
 package com.example.john.quizsurvey.Views;
 
+import com.example.john.quizsurvey.DataModels.Questionare;
+
+import java.util.ArrayList;
+
 /**
  * Created by Laura on 4/26/2018.
  */
 
 public class CreateMatchQuestion {
+    Questionare questionare;
+    int count = 2;
+    String ans = "";
+    ArrayList<String> match1list = new ArrayList<>();
+    ArrayList<String> match2list = new ArrayList<>();
+
+    
 }
