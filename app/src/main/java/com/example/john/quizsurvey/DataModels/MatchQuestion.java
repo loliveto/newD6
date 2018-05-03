c1e875a48c5abbcd50efb5a4c98e4eccbd9c7f44
loliveto <olivetolt@verizon.net>
2018-05-03 12:27:13 -0400

    matching complete

--- app/src/main/java/com/example/john/quizsurvey/DataModels/MatchQuestion.java
+++ app/src/main/java/com/example/john/quizsurvey/DataModels/MatchQuestion.java
@@ -4,5 +4,10 @@ package com.example.john.quizsurvey.DataModels;
  * Created by Laura on 4/18/2018.
  */
 
-public class MatchQuestion {
+public class MatchQuestion extends Question {
+    public MatchQuestion(String p)
+    {
+        prompt = p;
+        type = "MA";
+    }
 }
